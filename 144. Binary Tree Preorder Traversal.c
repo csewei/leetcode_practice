2a185ca1b8d0f3b99017bbdef19912e509c42bb6
csewei <126608616+csewei@users.noreply.github.com>
2025-04-30 17:13:08 +0800

    Update 144. Binary Tree Preorder Traversal.c

--- 144. Binary Tree Preorder Traversal.c
+++ 144. Binary Tree Preorder Traversal.c
@@ -14,3 +14,38 @@ int* preorderTraversal(struct TreeNode* root, int* returnSize){
     preOrder(root, ret, returnSize);
     return ret;
 }
+
+
+//    非遞迴
+
+int* preorderTraversal(struct TreeNode* root, int* returnSize) {
+    struct TreeNode** stack =
+        (struct TreeNode**)malloc(sizeof(struct TreeNode*) * 100);
+    
+    int* result = (int*)malloc(sizeof(int) * 100);
+    int top = -1;
+    *returnSize = 0;
+
+    if (root == NULL) {
+        free(stack);
+        return result;
+    }
+    
+    // 將根節點放入 stack
+    stack[++top] = root;
+
+    while (top >= 0) {
+        struct TreeNode* node = stack[top--];              // 中
+        result[(*returnSize)++] = node->val;
+
+        if (node->right){
+            stack[++top] = node->right;        // 右先進（後出）
+        } 
+        if (node->left){
+            stack[++top] = node->left;         // 左後進（先出）    
+        } 
+    }
+
+    free(stack);
+    return result;
+}
