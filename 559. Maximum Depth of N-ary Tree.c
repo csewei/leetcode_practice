b2b178abc9f5b178adc478c65471ea9fedc7d169
csewei <126608616+csewei@users.noreply.github.com>
2025-05-05 11:21:58 +0800

    Update 559. Maximum Depth of N-ary Tree.c

--- 559. Maximum Depth of N-ary Tree.c
+++ 559. Maximum Depth of N-ary Tree.c
@@ -14,3 +14,81 @@ int maxDepth(struct Node* root) {
 
 
 //方法2:BFS
+typedef struct QNode {
+    struct Node* treeNode;
+    struct QNode* next;
+} QNode;
+
+typedef struct Queue {
+    QNode* front;
+    QNode* rear;
+    int size;
+} Queue;
+
+/*** Queue 操作 ***/
+Queue* createQueue() {
+    Queue* q = (Queue*)malloc(sizeof(Queue));
+    q->front = q->rear = NULL;
+    q->size = 0;
+    return q;
+}
+
+int isEmpty(Queue* q) { return q->front == NULL; }
+
+void enqueue(Queue* q, struct Node* node) {
+    QNode* newNode = (QNode*)malloc(sizeof(QNode));
+    newNode->treeNode = node;
+    newNode->next = NULL;
+
+    if (q->rear == NULL) {
+        q->front = q->rear = newNode;
+    } else {
+        q->rear->next = newNode;
+        q->rear = newNode;
+    }
+    q->size++;
+}
+
+struct Node* dequeue(Queue* q) {
+    if (isEmpty(q))
+        return NULL;
+
+    QNode* temp = q->front;
+    struct Node* node = temp->treeNode;
+    q->front = q->front->next;
+    if (q->front == NULL)
+        q->rear = NULL;
+    free(temp);
+    q->size--;
+    return node;
+}
+
+void freeQueue(Queue* q) {
+    while (!isEmpty(q)) {
+        dequeue(q);
+    }
+    free(q);
+}
+
+int maxDepth(struct Node* root) {
+    if (root == NULL) {
+        return 0;
+    }
+
+    Queue* q = createQueue();
+    enqueue(q, root);
+    int floor = 0;
+    while (!isEmpty(q)) {
+        int levelSize = q->size;
+
+        for (int i = 0; i < levelSize; i++) {
+            struct Node* node = dequeue(q);
+            for (int j = 0; j < node->numChildren; j++) {
+                    enqueue(q, node->children[j]);
+            }
+        }
+        floor++;
+    }
+    freeQueue(q);
+    return floor;
+}
