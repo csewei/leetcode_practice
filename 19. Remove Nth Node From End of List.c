d645cd22282e155df85e8bad4f02c0b4490d20ff
csewei <126608616+csewei@users.noreply.github.com>
2025-10-30 17:11:21 +0800

    Update 19. Remove Nth Node From End of List.c

--- 19. Remove Nth Node From End of List.c
+++ 19. Remove Nth Node From End of List.c
@@ -1,27 +1,21 @@
 struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
     typedef struct ListNode ListNode;
-    ListNode* dummyhead = (ListNode*)malloc(sizeof(ListNode));
-    dummyhead->val = 0;
-    dummyhead->next = head;
+    ListNode* dummy = malloc(sizeof(ListNode));
+    dummy->val = 0;
+    dummy->next = head;
+    ListNode* fast = head;
+    ListNode* slow = dummy;
 
-    ListNode* fast = dummyhead;
-    ListNode* slow = dummyhead;
-
-    n++; // 讓fast先走n+1步
-    while (n && fast != NULL) {
-        n--;
+    for (int i = 0; i < n ; i++) {
         fast = fast->next;
     }
-    while (fast != NULL) { // fast走完n+1步後，fast和slow一起走
+    while(fast) {
         fast = fast->next;
         slow = slow->next;
-    } // 走完後slow指在要刪的節點的前一個節點
-    
-    ListNode *tmp = slow->next;  
-    slow->next = tmp->next;
-    free(tmp);      //釋放被刪掉節點占用的空間
-    
-    head = dummyhead->next;
-    free(dummyhead);
-    return head;
+    }
+    struct ListNode* temp;
+    temp = slow->next;
+    slow->next = temp->next;
+    free(temp);
+    return dummy->next;
 }
